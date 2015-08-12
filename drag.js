3c1ac852f01b63f75c47bd10e8b997709a1a1223
hzdzlwh <742783121@qq.com>
2015-08-12 19:17:42 +0800

    modify drag.js

--- drag.js
+++ drag.js
@@ -2,8 +2,10 @@ function drag(id){
 	var oDiv=document.getElementById(id);
 	var disX=0;
 	var disY=0;
-	oDiv.onmousedown=function()
+	oDiv.onmousedown=function(ev)
+
 	{
-		
+		disX=ev.pageX- oDiv.offsetLeft;
+		disY=ev.pageY- oDiv.offsetTop;
 	}
 }
